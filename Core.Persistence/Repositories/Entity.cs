db629ed989a911def8bda6da2cdf74435df4e02c
Furkan ARTAR <asosyalgenctvtr@gmail.com>
2023-11-06 00:18:42 +0300

    Add project files.

---
+++ Core.Persistence/Repositories/Entity.cs
@@ -0,0 +1,19 @@
+﻿namespace Core.Persistence.Repositories;
+
+public class Entity<TId>
+{
+    public TId Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }//nullable bu alanlar zorunlu olmadığı için null geçilebileceğini belirtiyoruz
+    public DateTime? DeletedDate { get; set; }//nullable
+
+    public Entity()
+    {
+        Id = default; //! id için değer verilmemişse mesela TId'nin tipi int ise 0 değerini vermesini istiyoruz
+    }
+
+    public Entity(TId id)
+    {
+        Id = id;
+    }
+}
